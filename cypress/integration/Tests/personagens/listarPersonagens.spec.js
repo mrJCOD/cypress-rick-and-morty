f9277b3856f44ace59dffeed5890fd880cb5f15c
mrJCOD <mauro_rodrigues15@hotmail.com>
2022-08-14 11:07:53 -0300

    first commit

---
+++ cypress/integration/Tests/personagens/listarPersonagens.spec.js
@@ -0,0 +1,45 @@
+
+
+describe('Listar todos os personagens', () => {
+
+    const baseUrl = "https://rickandmortyapi.com/api"
+
+    it('Listar todos os personagens - Sucesso', () => {
+
+        
+        cy.request({
+            method: 'GET',
+            url: baseUrl + '/character',
+            headers: { 'Accept-Language': 'pt-BR', },
+        }).then((responseGet)=>{
+
+            expect(responseGet.status).to.eq(200)
+            expect(responseGet.body).is.not.null
+            expect(responseGet.body.results[0].id).to.eq(1)
+            expect(responseGet.body.results[0].name).to.eq('Rick Sanchez')
+            expect(responseGet.body.results[15].name).to.eq('Amish Cyborg')
+
+        })
+        
+    })
+
+    it.only('Listar todos os personagens - Falha', () => {
+
+        cy.request({
+            method: 'GET',
+            url: baseUrl + '/characte',
+            failOnStatusCode: false,
+            headers: { 'Accept-Language': 'pt-BR', },
+        }).then((responseGet)=>{
+
+            expect(responseGet.status).to.eq(404)
+            expect(responseGet.body).is.not.null
+            expect(responseGet.body.error).to.eq('There is nothing here.')
+
+
+        })
+
+
+        
+    });
+});
---
+++ cypress/integration/Tests/personagens/listarUnicoPersonagem.spec.js
@@ -0,0 +1,68 @@
+
+
+
+
+describe('Listar Unico personagem', () => {
+    it('Listar um personagem - Sucesso', () => {
+        const baseUrl = "https://rickandmortyapi.com/api"
+        cy.request({
+            method: 'GET',
+            url: baseUrl + '/character/15',
+            headers: { 'Accept-Language': 'en-us', },
+        }).then((response) => {
+
+            expect(response.status).to.eq(200)
+            expect(response.body).is.not.null
+            expect(response.body.id).to.eq(15)
+            expect(response.body.name).to.eq('Alien Rick')
+            expect(response.body.status).to.eq('unknown')
+            expect(response.body.species).to.eq('Alien')
+            expect(response.body.type).to.eq('')
+            expect(response.body.gender).to.eq('Male')
+            expect(response.body.origin.name).to.eq('unknown')
+            expect(response.body.origin.url).to.eq('')
+            expect(response.body.location.name).to.eq('Citadel of Ricks')
+            expect(response.body.location.url).to.eq('https://rickandmortyapi.com/api/location/3')
+            expect(response.body.image).to.eq('https://rickandmortyapi.com/api/character/avatar/15.jpeg')
+            expect(response.body.url).to.eq('https://rickandmortyapi.com/api/character/15')
+            expect(response.body.created).to.eq('2017-11-04T20:56:13.215Z')
+        })
+
+    });
+
+    it('Listar um personagem que nao existe - Falha', () => {
+        const baseUrl = "https://rickandmortyapi.com/api"
+        cy.request({
+            method: 'GET',
+            url: baseUrl + '/character/3209',
+            failOnStatusCode: false,
+            headers: { 'Accept-Language': 'en-us', },
+        }).then((response) => {
+
+            expect(response.status).to.eq(404)
+            expect(response.body).is.not.null
+            expect(response.body.error).to.eq('Character not found')
+            
+        })
+
+    });
+
+    it('Listar um personagem endpoint errado - Falha', () => {
+        const baseUrl = "https://rickandmortyapi.com/api"
+        cy.request({
+            method: 'GET',
+            url: baseUrl + '/character/perso1',
+            failOnStatusCode: false,
+            headers: { 'Accept-Language': 'en-us', },
+        }).then((response) => {
+
+            expect(response.status).to.eq(500)
+            expect(response.body).is.not.null
+            expect(response.body.error).to.eq('Hey! you must provide an id')
+            
+        })
+
+    });
+
+
+});
